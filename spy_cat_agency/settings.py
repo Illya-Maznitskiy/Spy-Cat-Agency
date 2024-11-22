ef9a8deba95a8920b0087701f03fd7912530f5f3
Illya Maznitskiy <comercaleuros@gmail.com>
2024-11-22 14:24:27 +0200

    Fix PEP8 issues | Add installed apps in settings.py

--- spy_cat_agency/settings.py
+++ spy_cat_agency/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-*+6+az9yezn2(!-mid#hdgwp@^e-jdchiqxsb87)lrh6motv)p"
+SECRET_KEY = (
+    "django-insecure-*+6+az9yezn2(!-mid#hdgwp@^e-jdchiqxsb87)lrh6motv)p"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +39,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "spy_cat_agency",
+    "spy_cat_app",
 ]
 
 MIDDLEWARE = [
@@ -86,16 +91,20 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+        "NAME": "django.contrib.auth.password_validation."
+        "UserAttributeSimilarityValidator",
     },
     {
-        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+        "NAME": "django.contrib.auth.password_validation."
+        "MinimumLengthValidator",
     },
     {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+        "NAME": "django.contrib.auth.password_validation."
+        "CommonPasswordValidator",
     },
     {
-        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+        "NAME": "django.contrib.auth.password_validation."
+        "NumericPasswordValidator",
     },
 ]
 
